499f1406d920e8f00b6a946e3013c8cc8c7e4032
Vijay <vijay.annamalai001@gmail.com>
2024-01-15 20:36:54 +0530

    Implemented usestate hooks Concept

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,40 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Header = () => {
   
-  function handleNameChange(){
-    const names = ["React","Angular","Spring"]
-    const int = Math.floor(Math.random()*3);
-    return names[int];
-  }
+     const  [items, setName] = useState([{
+      id:1,
+      checked: true,
+      item : "Playing Euro Truck"
+     },
+     {
+      id:2,
+      checked: true,
+      item : "Playing GTA %"
+     },
+     {
+      id:3,
+      checked: true,
+      item : "Playing NFS"
+     },
+     {
+      id:4,
+      checked: true,
+      item : "Playing Rally Racing"
+     }
+     ]);
+
   return (
-    <p>Let's Learn {handleNameChange()} Framework</p>
+   <main>
+    <ul>
+       {items.map( (item) => (<li>
+        <input type='checkbox' checked={item.checked}/>
+        <label>{item.item}</label>
+        <button>Remove</button>
+       </li>))}
+    </ul>
+   </main>
   )
 }
 
